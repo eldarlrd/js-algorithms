8a64af7ed3a6a0ab0c9b0deab0dd700741ec505c
eldarlrd <eldarlrd@protonmail.com>
2023-03-16 13:49:15 +0400

    feat: Sum Multiples of 3 and 5

---
+++ sum-multiples.js
@@ -0,0 +1,9 @@
+const multiplesOf3and5 = number => {
+  let arr = [];
+  let sum = 0;
+  for (let i = 1; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0)
+    arr.push(i);
+  } for (let i of arr) sum += i;
+  return sum;
+}

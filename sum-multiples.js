442c85cbb5c722a5e2672e176c859f3d251e318a
eldarlrd <eldarlrd@protonmail.com>
2023-03-18 13:20:40 +0400

    refactor: Indentation

--- sum-multiples.js
+++ sum-multiples.js
@@ -3,7 +3,7 @@ const multiplesOf3and5 = number => {
   let sum = 0;
   for (let i = 1; i < number; i++) {
     if (i % 3 === 0 || i % 5 === 0)
-    arr.push(i);
+      arr.push(i);
   } for (let i of arr) sum += i;
   return sum;
 }

ce5ea91e2de76c93948bbadf10d6fe9bbd493471
eldarlrd <eldarlrd@pm.me>
2023-05-11 21:05:55 +0400

    feat: Header & Favicon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,23 @@
 import {
+  Box,
   Heading,
+  Highlight
 } from "@chakra-ui/react";
 
 export const Header = () => {
   return (
-    <Heading as='h1'>
-      Interactive JavaScript Algorithms
-    </Heading>
+    <Box bg='yellow.100' py='8' px='8'>
+      <Heading as='h1' textAlign={['center', 'left']}>
+        <Highlight
+          query='JavaScript'
+          styles={{
+            px: 2,
+            py: 1,
+            rounded: 4,
+            bg: 'yellow.300'
+          }}> Interactive JavaScript Algorithms
+        </Highlight>
+      </Heading>
+    </Box>
   );
 }

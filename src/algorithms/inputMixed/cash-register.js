e50e15ee252612242db024309bd7498d4d91142c
eldarlrd <eldarlrd@pm.me>
2023-06-07 15:34:21 +0400

    feat: Inputs with Mixed Arguments

--- src/algorithms/inputMixed/cash-register.js
+++ src/algorithms/inputMixed/cash-register.js
@@ -2,16 +2,14 @@ export const checkCashRegisterObj = {
   name: 'Cash Register',
   placeholder: 'Price, Cash, Cash in Drawer',
   myFunc(inputArr) {
-    console.log(inputArr)
-    console.log(inputArr[2])
     // Type Checking
+    const error =
+  'ERROR: Inputs must be (num, num, arr[9×[str, num]])';
+    if (inputArr.length !== 20) return error;
+    const cid = JSON.parse(inputArr.slice(2).join());
     const price = +inputArr[0];
     const cash = +inputArr[1];
-    const cid = inputArr[2];
-    const error =
-  'ERROR: Inputs must be (number, number, array[9 × [string, number]])';
-    if (inputArr.length !== 3 ||
-        price < 0 || cash < 0 ||
+    if (price < 0 || cash < 0 ||
         !price || !cash ||
         cid.length !== 9)
       return error;
@@ -34,8 +32,9 @@ export const checkCashRegisterObj = {
     // Declaring the Change
     let change = cash - price;
     if (change < 0)
-      return
-        `The customer owes you $${-change.toFixed(2)}`;
+      return (
+        `The customer owes you $${-change.toFixed(2)}`
+      );
     // Transforming the Drawer Array into an Object
     const drawer = cid.reduce((drwObj, curr) => {
       drwObj[curr[0]] = curr[1];
@@ -44,11 +43,12 @@ export const checkCashRegisterObj = {
     }, {total: 0});
     // Checking for the Exact Change
     if (drawer.total === change)
-      return {status: 'CLOSED', change: cid};
+      return `status: CLOSED => change: ${cid.join(' | ')}`;
     // Declaring the Change Array
     const changeArr = currArr.reduce((drwObj, curr) => {
       let tmpVal = 0;
-      while (drawer[curr.name] > 0 && change >= curr.val) {
+      while (drawer[curr.name] > 0 &&
+             change >= curr.val) {
         drawer[curr.name] -= curr.val;
         tmpVal += curr.val;
         change -= curr.val;
@@ -60,17 +60,18 @@ export const checkCashRegisterObj = {
       } return drwObj;
     }, []);
     // Checking for the Due Change
+    if (changeArr.length === 0)return 'status: OPEN';
     return change <= 0
-      ? {status: 'OPEN', change: changeArr}
-      : {status: 'INSUFFICIENT_FUNDS', change: []};
+      ? `status: OPEN => change: ${changeArr.join(' | ')}`
+      : 'status: INSUFFICIENT_FUNDS';
   },
   raw:
 `const checkCashRegister = (price, cash, cid) => {
   // Type Checking
+  const error =
+'ERROR: Inputs must be (num, num, arr[9×[str, num]])';
   price = +price;
   cash = +cash;
-  const error =
-'ERROR: Inputs must be (number, number, array[9 × [string, number]])';
   if (price < 0 || cash < 0 ||
       !price || !cash ||
       cid.length !== 9)
@@ -94,8 +95,9 @@ export const checkCashRegisterObj = {
   // Declaring the Change
   let change = cash - price;
   if (change < 0)
-    return
-      \`The customer owes you $\${-change.toFixed(2)}\`;
+    return (
+      \`The customer owes you $\${-change.toFixed(2)}\`
+    );
   // Transforming the Drawer Array into an Object
   const drawer = cid.reduce((drwObj, curr) => {
     drwObj[curr[0]] = curr[1];
@@ -108,7 +110,8 @@ export const checkCashRegisterObj = {
   // Declaring the Change Array
   const changeArr = currArr.reduce((drwObj, curr) => {
     let tmpVal = 0;
-    while (drawer[curr.name] > 0 && change >= curr.val) {
+    while (drawer[curr.name] > 0 &&
+           change >= curr.val) {
       drawer[curr.name] -= curr.val;
       tmpVal += curr.val;
       change -= curr.val;
@@ -120,6 +123,7 @@ export const checkCashRegisterObj = {
     } return drwObj;
   }, []);
   // Checking for the Due Change
+  if (changeArr.length === 0) return 'status: OPEN';
   return change <= 0
     ? {status: 'OPEN', change: changeArr}
     : {status: 'INSUFFICIENT_FUNDS', change: []};

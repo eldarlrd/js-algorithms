4d9164cbe6e7c3a860660bddfa65416b2d528212
eldarlrd <eldarlrd@pm.me>
2025-04-18 13:04:24 +0400

    feat(sort): tower of hanoi

--- src/algorithms/sortFunc/towerOfHanoi.js
+++ src/algorithms/sortFunc/towerOfHanoi.js
@@ -1,43 +1,46 @@
 export const towerOfHanoiObj = {
   name: 'Tower of Hanoi',
   placeholder: 'N Discs',
-  myFunc(inputArr) {
-    const result = towerOfHanoiObj.getMoves(inputArr);
-    return typeof result === 'string' ? result : (
-        towerOfHanoiObj.formatMoves(result)
-      );
+  myFunc(n) {
+    n = +n;
+    const src = 'A';
+    const tgt = 'B';
+    const aux = 'C';
+
+    if (n < 1 || n % 1 !== 0) return 'ERROR: Input must be a natural number';
+    else if (n > 10) return 'ERROR: Input value too high';
+
+    const moves = towerOfHanoiObj.getMoves(n, src, tgt, aux);
+    return moves.map(([from, to]) => `[${from} → ${to}]`).join(', ');
   },
-  getMoves(inputArr) {
-    const n = Math.abs(inputArr[0]);
-    const src = inputArr[1];
-    const tgt = inputArr[2];
-    const aux = inputArr[3];
-
-    if (n <= 0 || n % 1 !== 0) return 'ERROR: Input must be a natural number';
-    else if (n > 1000) return 'ERROR: Input value too high';
-
+  getMoves(n, src, tgt, aux) {
     if (n === 1) return [[src, tgt]];
 
     return [
-      ...towerOfHanoiObj.getMoves([n - 1, src, aux, tgt]),
+      ...towerOfHanoiObj.getMoves(n - 1, src, aux, tgt),
       [src, tgt],
-      ...towerOfHanoiObj.getMoves([n - 1, aux, tgt, src])
+      ...towerOfHanoiObj.getMoves(n - 1, aux, tgt, src)
     ];
   },
-  formatMoves(moves) {
-    return `[${moves.map(([from, to]) => `['${from}', '${to}']`).join(', ')}]`;
-  },
-  raw: `const towerOfHanoi = n => {
-  const src = 'Source';
-  const tgt = 'Target';
-  const aux = 'Auxiliary';
+  raw: `// Assemble the Tower of Hanoi
+const towerOfHanoi = n => {
+  const src = 'A';
+  const tgt = 'B';
+  const aux = 'C';
 
+  const moves = getMoves(n, src, tgt, aux);
+  return moves.map(([from, to]) =>
+    \`[\${from} → \${to}]\`).join(', ');
+}
+
+// Calculate the Moves
+const getMoves = (n, src, tgt, aux) => {
   if (n === 1) return [[src, tgt]];
 
   return [
-    ...towerOfHanoi(n - 1, src, aux, tgt),
+    ...getMoves(n - 1, src, aux, tgt),
     [src, tgt],
-    ...towerOfHanoi(n - 1, aux, tgt, src)
+    ...getMoves(n - 1, aux, tgt, src)
   ];
 }`
 };

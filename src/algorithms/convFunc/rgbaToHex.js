0666f533e71cd60487b37896fcef3c72e3b4dbe5
eldarlrd <eldarlrd@pm.me>
2025-10-20 17:10:28 +0400

    fix(conv-func): rgbaToHex num conversion

--- src/algorithms/convFunc/rgbaToHex.js
+++ src/algorithms/convFunc/rgbaToHex.js
@@ -4,7 +4,7 @@ export const rgbaToHexObj = {
   myFunc(inputArr) {
     const [r, g, b, a = 1] = inputArr;
     const colors =
-      a < 1 ? [r, g, b, Math.round(a * 255)] : [r, g, b];
+      a < 1 ? [+r, +g, +b, Math.round(a * 255)] : [+r, +g, +b];
 
     if (colors.some(c => c > 255 || c < 0 || c % 1 !== 0))
       return 'ERROR: Inputs out of bounds';
